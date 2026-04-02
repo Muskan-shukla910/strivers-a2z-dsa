a0875ae8139ff64d31bbc9cc7bf6cef3f612de4a
Muskan Shukla <muskanshukla187@gmail.com>
2026-04-02 22:08:50 +0530

    Input_Output

---
+++ basicsOfJava/inputOutput.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class inputOutput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Your first Number :");
+        int firstNo=sc.nextInt();
+        System.out.print("Enter Your Secound Number :");
+        int SecoundNo=sc.nextInt();
+        int add=firstNo+SecoundNo;
+        System.out.println("Addition of two nunbers is :"+add);
+
+
+        BigInteger bg=sc.nextBigInteger();
+       System.out.println("big number "+bg);
+
+    //    System.out.println("Enter the value of flag");
+       boolean flag=sc.nextBoolean();
+       System.out.println(flag);
+       
+
+    }
+    
+}
