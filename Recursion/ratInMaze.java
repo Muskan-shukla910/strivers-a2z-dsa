dfcb7657d5f144a5b9bccd4c80166814cbc0f5de
Muskan Shukla <muskanshukla187@gmail.com>
2026-04-29 14:46:37 +0530

    Rat In MAze

--- Recursion/ratInMaze.java
+++ Recursion/ratInMaze.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class ratInMaze {
-    public static void rec(int row, int col, int[][] maze, String path, List<String> ans, int n, boolean[][] visited) {
+    public static void  rec(int row, int col, int[][] maze, String path, List<String> ans, int n, boolean[][] visited) {
         // base case
-        if (row < 0 || col < 0 || row > n || col > n) {
+        if (row < 0 || col < 0 || row >= n || col >= n) {
             return;
         }
         // next base case(main)
@@ -15,9 +15,23 @@ public class ratInMaze {
         // recursion
         visited[row][col] = true;
         // up
+        if(row-1>=0 && maze[row-1][col]==1 && !visited[row-1][col]){
+            rec(row-1, col, maze, path+"U", ans, n, visited);
+        }
         // down
+        if(row+1<n && maze[row+1][col]==1 && !visited[row+1][col] ){
+            rec(row+1, col, maze, path+"D", ans, n, visited);
+        }
+
         // left
+        if(col+1<n && maze[row][col+1]==1 && !visited[row][col+1]){
+            rec(row, col+1, maze, path+"L", ans, n, visited);
+        }
         // right
+        if(col-1>=0 && maze[row][col-1]==1 && !visited[row][col-1]){
+            rec(row, col-1, maze, path+"R", ans, n, visited);
+        }
+        visited[row][col]=false;
     }
 
     public static void main(String[] args) {
@@ -32,8 +46,10 @@ public class ratInMaze {
         int n = maze.length;
         boolean visited[][] = new boolean[n][n];
         if( maze[0][0] == 1) {
-            rec(maze, 0, 0, " ", ans, n, visited);
+            rec( 0, 0,maze, " ", ans, n, visited);
+            // System.out.println(ans);
         }
+        System.out.println(ans);
 
     }
 
